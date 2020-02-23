1652ba727ca50de5eefff3b62ca610b48b54ef2d
Gogagogin <georgec.gil@gmail.com>
2020-02-23 17:30:06 +0300

    First commit

--- Task3/src/by/gil/Main.java
+++ Task3/src/by/gil/Main.java
@@ -3,6 +3,12 @@ package by.gil;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int s = (int)(Math.random()*100);
+	if (s%10 == 7) {
+        System.out.println(s + " Оканчивается на 7");
+    } else {
+        System.out.println(s + " Не Оканчивается на 7");
+    }
+
     }
 }

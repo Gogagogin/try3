aa0bc62303f065248ff3ec1213a806cb6f892d49
Gogagogin <georgec.gil@gmail.com>
2020-02-23 17:56:14 +0300

    First Commit

---
+++ Task4/src/by/gil/Main.java
@@ -0,0 +1,16 @@
+package by.gil;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 4, b = 8, r = 16;
+        if ((a*a+b*b) <= (r*r)){
+            System.out.println("Можно");
+        } else {
+            System.out.println("Нельзя");
+        }
+    }
+}

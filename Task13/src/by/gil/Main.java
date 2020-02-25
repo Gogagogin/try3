2f67761ab820c0ac379638b6b4768d09b2dce7c2
Gogagogin <georgec.gil@gmail.com>
2020-02-25 21:27:50 +0300

    First Commit

--- Task13/src/by/gil/Main.java
+++ Task13/src/by/gil/Main.java
@@ -3,15 +3,16 @@ package by.gil;
 public class Main {
 
     public static void main(String[] args) {
+        String a = "Число простое";
 
-            int number = 493;
+            int number = 491;
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
-                    System.out.println("Число не простое, а имеет как минимум делитель :" + i);
+                    a = "Число не простое, а имеет как минимум делитель :" + i;
                     break;
                 }
             }
-            System.out.println("Число простое");
+            System.out.println(a);
         }
 }
 

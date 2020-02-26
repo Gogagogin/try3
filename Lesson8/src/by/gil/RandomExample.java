f541f5725ab478203277d03bbeffe7aaa21b3669
Gogagogin <georgec.gil@gmail.com>
2020-02-26 20:03:28 +0300

    Fisrt commit

---
+++ Lesson8/src/by/gil/RandomExample.java
@@ -0,0 +1,28 @@
+package by.gil;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomExample {
+
+    public static void main(String[] args) {
+
+        int[] ints = new int[10];
+        for (int i = 0; i < ints.length; i++) {
+            Random random = new Random();
+            ints[i] = random.nextInt(100);
+            //System.out.println( ints[i]);
+        }
+        System.out.println(Arrays.toString(ints));
+        System.out.println(searchNumber(5, ints));
+
+    }
+
+
+    static int searchNumber(int target, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) return i;
+        }
+        return -1;
+    }
+}
